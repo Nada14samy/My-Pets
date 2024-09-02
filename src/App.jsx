d50dafbaba2b0c9e1a0b9978e9bc2d1939b5c258
Nada14samy <nm5135787@gmail.com>
2024-09-03 00:36:13 +0200

    change files css and ADD scroll animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/home/HomePage.jsx";
-import { SignUp } from "./sections/index";
-import Login from "./pages/login/Login";
+import SignUp from "./Pages/SignUp/SignUp.jsx";
+import Login from "./Pages/Login/Login.jsx";
+import VerifyEmail from "./Pages/VerifyEmail/VerifyEmail.jsx";
 
 const App = () => {
   return (
     <>
      <Routes>
-       {/*     //   
-        
-      //   <Route path="/login" element={<Login />} />
-      */}
           <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/verifyEmail" element={<VerifyEmail />} />
       </Routes> 
-      
     </>
   );
 };
